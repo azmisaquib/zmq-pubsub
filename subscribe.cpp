a63f20c54b240474daad38e190414d37d719d032
saquib <saquib@21e8.nz>
2021-02-11 11:44:10 +1300

    Commented code and removed unnecessary includes

--- subscribe.cpp
+++ subscribe.cpp
@@ -1,29 +1,28 @@
-#include <cstring>
-#include <errno.h>
-#include <fstream>
+// Created By Saquib Azmi on 11/02/2021
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string>
-#include <sys/types.h>
-#include <chrono>
-#include <thread>
 #include "zmq/zmq.hpp"
 
 
 int main( int argc, char *argv[] )
 {
+    // Create ZMQ Context
     zmq::context_t context ( 1 );
+    // Create the Subscribe socket
     zmq::socket_t subscriber ( context, ZMQ_SUB );
+    // Connect to a tcp socket
     subscriber.connect( "tcp://localhost:5556" );
+    // Set the socket option to subscribe
     subscriber.setsockopt( ZMQ_SUBSCRIBE, "", 0 );
 
     // infinite loop to receive messages
     for ( int i = 1; i > 0; i++ )
     {
+        // Receive the message and convert to string
         zmq::message_t update;
         subscriber.recv( &update );
         std::string msg = update.to_string();
+        // Print the message
         std::cout << "Num: " << i << ", message: " << msg << std::endl;
     }
 
